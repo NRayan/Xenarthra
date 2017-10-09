168445c2a0582cfbab8241a7329a5d13cbae58a7
Norton Rayan Meira de Oliveira <norton_rayan@hotmail.com>
2017-10-09 18:18:46 -0300

    V0.06

--- Xenarthra/Xenarthra/Views/Catalogo.xaml.cs
+++ Xenarthra/Xenarthra/Views/Catalogo.xaml.cs
@@ -3,34 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xenarthra.Models;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using Xenarthra.Models;
+
 
 namespace Xenarthra.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Catalogo : ContentPage
+    public partial class Catalogo : TabbedPage
     {
-        public Catalogo()
+        public Catalogo ()
         {
             InitializeComponent();
             this.BindingContext = this;
-        }        
+        }
 
         Animal pxnomeanimais = new Animal();
         public List<Animal> pxNomeAnimais
         {
             get
             {
-                return  pxnomeanimais.ListarNomeAnimais().OrderBy(x=>x.ani_Nome).ToList();
+                return pxnomeanimais.ListarNomeAnimais().OrderBy(x => x.ani_Nome).ToList();
             }
         }
 
-        private void sbAnimais_SearchButtonPressed(object sender, EventArgs e)
-        {
-            
-        }
     }
 }
