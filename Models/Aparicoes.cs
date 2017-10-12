da7cf82c81344074b4ccaf3c8b509cab9bc14cd2
Arthur Thomazette <atrmustd@gmail.com>
2017-10-12 02:15:25 -0300

    DAL/MODELS/WEBSERVICE

---
+++ Models/Aparicoes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Aparicoes
+    {
+        public int apa_ID { get; set; }
+        public int apa_ID_USU { get; set; }
+        public int apa_ID_ANI { get; set; }
+        public string apa_COMENTARIO { get; set; }
+        public bool apa_Validado { get; set; }
+        public Decimal apa_LATITUDE { get; set; }
+        public Decimal apa_LONGITUDE { get; set; }
+    }
+}
---
+++ WebService/WS1.asmx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using Models;
+using DAL;
+
+namespace WebService
+{
+    /// <summary>
+    /// Summary description for WS1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WS1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public void InserirAnimal (Animais A)
+        {
+            animalDAL ADAL = new animalDAL();
+            A = new Animais();
+
+            ADAL.InserirAnimal(A);  
+        }
+        [WebMethod]
+        public List<Animais> ConsultaAnimais()
+        {
+            animalDAL ADAL = new animalDAL();
+
+            return ADAL.ListarAnimais();
+        }
+        [WebMethod]
+        public Animais BuscarAnimalCodigo(int Codigo)
+        {
+            animalDAL ADAL = new animalDAL();
+            Animais A = new Animais();
+
+            A = ADAL.BuscarAnimalCodigo(Codigo);
+
+
+            return A;
+        }
+    }
+}
