b5e8a438241cfdc5ca2e070601ea91d93ab84e9c
Luiz Munhoz JR <luizmunhoz.jr@gmail.com>
2017-11-14 17:31:49 -0200

    Update Web

--- Models/Aparicoes.cs
+++ Models/Aparicoes.cs
@@ -12,12 +12,13 @@ namespace Models
         public string apa_Comentario { get; set; }
         public string apa_ComentarioADM { get; set; }
         public DateTime apa_Data { get; set; }
-        public bool apa_status{ get; set; }
+        public int apa_status{ get; set; }
         public Decimal apa_Latitude { get; set; }
         public Decimal apa_Longitude { get; set; }
-        public Byte apa_IMG { get; set; }
+        public Byte[] apa_IMG { get; set; }
         public int apa_tipo_animal { get; set; }
         public int apa_ID_USU { get; set; }
         public int apa_ID_ANI { get; set; }
+        public string apa_Nome { get; set; }
     }
 }
